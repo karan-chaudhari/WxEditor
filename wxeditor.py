79ceb31447ea68e28ec543edb07570917aff42a3
Karan Chaudhari <chaudharik353@gmail.com>
2018-09-25 00:15:11 +0530

    Add Menuicons

--- wxeditor.py
+++ wxeditor.py
@@ -1,5 +1,7 @@
 import wx
 
+MENU_ICON = 1
+
 class WxEditor(wx.Frame):
     def __init__(self,*args,**kwargs):
         super(WxEditor,self).__init__(*args,**kwargs)
@@ -10,22 +12,55 @@ class WxEditor(wx.Frame):
 
         filemenu = wx.Menu()
         menubar.Append(filemenu,'File')
-        filemenu.Append(wx.ID_NEW,'New\tCtrl+N')
-        filemenu.Append(wx.ID_OPEN,'Open\tCtrl+O')
+
+        newi = wx.MenuItem(filemenu,MENU_ICON,'New\tCtrl+N')
+        newi.SetBitmap(wx.Bitmap('menuicons/new.png'))
+        filemenu.Append(newi)
+        
+        openi = wx.MenuItem(filemenu,MENU_ICON,'Open\tCtrl+O')
+        openi.SetBitmap(wx.Bitmap('menuicons/open.png'))
+        filemenu.Append(openi)
+
         filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_SAVE,'Save\tCtrl+S')
-        filemenu.Append(wx.ID_SAVEAS,'Save As\tCtrl+Shift+S')
+        
+        savei = wx.MenuItem(filemenu,MENU_ICON,'Save\tCtrl+S')
+        savei.SetBitmap(wx.Bitmap('menuicons/save.png'))
+        filemenu.Append(savei)
+
+        save_asi = wx.MenuItem(filemenu,MENU_ICON,'Save As\tCtrl+Shift+S')
+        save_asi.SetBitmap(wx.Bitmap('menuicons/save-as.png'))
+        filemenu.Append(save_asi)
+
         filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,'Exit\tAlt+F4')
+        
+        exiti = wx.MenuItem(filemenu,MENU_ICON,'Exit\tAlt+F4')
+        exiti.SetBitmap(wx.Bitmap('menuicons/exit.png'))
+        filemenu.Append(exiti)
 
         editmenu = wx.Menu()
         menubar.Append(editmenu,'Edit')
-        editmenu.Append(wx.ID_UNDO,'Undo\tCtrl+Z')
-        editmenu.Append(wx.ID_REDO,'Undo\tCtrl+Y')
+
+        undoi = wx.MenuItem(filemenu,MENU_ICON,'Undo\tCtrl+Z')
+        undoi.SetBitmap(wx.Bitmap('menuicons/undo.png'))
+        editmenu.Append(undoi)
+
+        redoi = wx.MenuItem(filemenu,MENU_ICON,'Redo\tCtrl+Y')
+        redoi.SetBitmap(wx.Bitmap('menuicons/redo.png'))
+        editmenu.Append(redoi)
+
         editmenu.AppendSeparator()
-        editmenu.Append(wx.ID_CUT,'Undo\tCtrl+X')
-        editmenu.Append(wx.ID_COPY,'Undo\tCtrl+C')
-        editmenu.Append(wx.ID_PASTE,'Undo\tCtrl+V')
+
+        cuti = wx.MenuItem(filemenu,MENU_ICON,'Cut\tCtrl+X')
+        cuti.SetBitmap(wx.Bitmap('menuicons/cut.png'))
+        editmenu.Append(cuti)
+
+        copyi = wx.MenuItem(filemenu,MENU_ICON,'Copy\tCtrl+C')
+        copyi.SetBitmap(wx.Bitmap('menuicons/copy.png'))
+        editmenu.Append(copyi)
+
+        pastei = wx.MenuItem(filemenu,MENU_ICON,'Paste\tCtrl+V')
+        pastei.SetBitmap(wx.Bitmap('menuicons/paste.png'))
+        editmenu.Append(pastei)
 
         self.SetMenuBar(menubar)
 
